b7e3dd612bf1dfffbdbb0b5afcea700bb1f4dc3b
The Developer Life <hello@thedeveloper.life>
2024-03-31 18:16:38 +0100

    test: Checking request headers

--- src/main/java/life/thedeveloper/smarttouch/infrastructure/web/ContactController.java
+++ src/main/java/life/thedeveloper/smarttouch/infrastructure/web/ContactController.java
@@ -1,18 +1,26 @@
 package life.thedeveloper.smarttouch.infrastructure.web;
 
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
 import jakarta.validation.Valid;
 import life.thedeveloper.smarttouch.domain.model.Contact;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContactController extends Controller {
 
     @PostMapping("/contacts")
-    public ResponseEntity<String> createContact(@Valid @RequestBody Contact contact) {
-        return ResponseEntity.ok("Contact created: " + contact);
+    public ResponseEntity<Map<String, Object>> createContact(@Valid @RequestBody Contact contact, @RequestHeader Map<String, String> headers) {
+        var result = ImmutableMap.<String, Object>builder()
+                .put("contact", contact)
+                .put("headers", headers)
+                .build();
+        return ResponseEntity.ok(result);
     }
-    
+
 }
