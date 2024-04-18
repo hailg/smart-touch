109a7c9fabe7c63795df09e31b038c93508d5347
The Developer Life <hello@thedeveloper.life>
2024-04-18 22:12:05 +0100

    Disable tests

--- src/test/java/life/thedeveloper/smarttouch/SmartTouchApplicationTests.java
+++ src/test/java/life/thedeveloper/smarttouch/SmartTouchApplicationTests.java
@@ -1,13 +1,13 @@
-package life.thedeveloper.smarttouch;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SmartTouchApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package life.thedeveloper.smarttouch;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SmartTouchApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
